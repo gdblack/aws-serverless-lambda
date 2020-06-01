339864cc1a192aa6ebdd7059d307110c9b56265f
Grant <gdblackx@me.com>
2020-06-01 14:15:17 -0400

    added the ability to use SSM parameters as environmental parameters within our lambda

--- AWSLambdaTest/AWSLambda1/Function.cs
+++ AWSLambdaTest/AWSLambda1/Function.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using AWSLambda1.Domain;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -19,9 +20,16 @@ namespace AWSLambda1
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async Task<string> FunctionHandler(string input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            // In order to standardize your parameters system wide you should store any environment variables
+            // in the SSM parameter store so you don't have to manage parameters for each lambda function.
+
+            var client = new AwsParameterStoreClient(Amazon.RegionEndpoint.USEast1);
+
+            var value = await client.GetValueAsync("Test-Parameter");
+
+            return value?.ToUpper();
         }
     }
 }
